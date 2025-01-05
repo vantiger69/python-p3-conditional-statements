25b4d77c5e240fd20716438238ffd3f529744eff
vantiger69 <vantiger817@gmail.com>
2025-01-05 10:19:15 +0300

    complete_lab

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,80 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+      return"Access granted"  
+     
+    else:
+     
+     return"Access denied"
 
+admin_login("sudo","12345")
+admin_login("admin","12345")
+admin_login("ADMIN","12345")
+
+    
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if  temperature < 40 :
+       return "It's brisk out there!"
+    elif temperature > 40 and temperature < 65 :
+       return "It's a little chilly out there!"
+    elif temperature > 85 :
+       return "It's too dang hot out there!"
+    else:
+       print("It's perfect out there!")
+hows_the_weather(39)
+hows_the_weather(42)
+hows_the_weather(99)
+hows_the_weather(57)
+   
 
 def fizzbuzz(num):
     # your code here
-    pass
+
+    if num % 3 == 0 and num % 5 == 0:
+      return "FizzBuzz"
+    elif num % 3 == 0:
+     return "Fizz"
+    elif num % 5 == 0:
+      return "Buzz"
+    else:
+      return num
+ 
+fizzbuzz(1)
+fizzbuzz(2)
+fizzbuzz(3)
+fizzbuzz(4)
+fizzbuzz(5)
+fizzbuzz(15)
+
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+" or operation == "-" or operation == "/" or operation == "*":
+     if operation == "+":
+      return num1 + num2
+     elif operation == "-":
+      return num1 - num2
+     elif operation == "*":
+      return num1 * num2
+     elif operation == "/":
+        if num2!=0:
+         return num1 / num2
+        else:
+          return "Cannot divide by zero"
+    else:
+         return"None"
+answer = calculator("+",1,1)
+print(answer)
+answer = calculator("-",3,1)
+print(answer)
+
+answer = calculator("*",3,2)
+print(answer)
+
+answer = calculator("/",4,2)
+print(answer)
+
+answer = calculator("nope",4,2)
+print(answer)
